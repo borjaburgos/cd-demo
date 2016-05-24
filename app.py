ddaad65cf0f43cc24d4610517b5b9deb8fdbec1b
Borja Burgos <borja@tutum.co>
2016-05-24 00:23:45 -0400

    change image

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}<br/>" \
-           "<img src=https://s3.amazonaws.com/f.cl.ly/items/3B2Q1M1x0p2R030b4122/docker_cloud.png>"
+           "<img src=https://s3.amazonaws.com/f.cl.ly/items/3f3Q0i2b0S252A28150n/docker_cloud.png>"
     return html.format(name=os.getenv('NAME', "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
